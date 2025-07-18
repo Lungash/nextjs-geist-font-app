0c93cfc8ab1a6f942800ae82a384a2cf8c933268
Lungash <59872127+Lungash@users.noreply.github.com>
2025-07-18 09:58:38 -0400

    Add .next/static/chunks/src_app_import_page_tsx_a3165332._.js 1752847118083

---
+++ .next/static/chunks/src_app_import_page_tsx_a3165332._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_13eda050._.js",
+  "static/chunks/node_modules_d2fa5377._.js"
+],
+    source: "dynamic"
+});
